2bfe1a1d272f7bf6512f6fc53f86f6dfd3eca1ee
Edgar <edgar.villegas4062@alumnos.udg.mx>
2026-03-06 08:22:57 -0600

    Add files via upload

---
+++ Arreglos/arreglos.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+
+
+float temp1, temp2;
+float temp[24];
+int dummy;
+
+printf("Direccion de memoria de 2 flotantes: &temp1: %i,\n&temp2 %i!\n", &temp1, &temp2);
+printf("Direccion de temp[24]: %i,\n", temp);
+printf("Direccion de &temp[23]: %i,\n", &temp[23]);
+printf("Direccion de &dummy[24]: %i,\n", &dummy);
+temp[0]= 20.5;
+temp[1]= 19.2;
+printf("%d", temp[0]);
+
+return 0;
+}
